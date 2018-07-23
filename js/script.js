a17ad506071fcd79f1de056c2067ebf7fc85b68f
Валера <valeravilks@gmail.com>
2018-07-23 10:47:30 +0300

    Blur working site name

--- js/script.js
+++ js/script.js
@@ -1,7 +1,26 @@
+
+$(document).ready(function() {
+    var offset = $('.name_and_description').offset();
+    console.log(offset);
+    var pad_r = $(window).width() - 54 - $('.name_and_description').width() - offset.left;
+    var pad_b = $(".header").height() - 30 - $('.name_and_description').height() - offset.top;
+    $('.header_blur').css({
+        "padding-left": offset.left,
+        "padding-top": offset.top,
+        "padding-right": pad_r,
+        "padding-bottom": pad_b
+    });
+});
+
 $(window).resize(function() {
     var offset = $('.name_and_description').offset();
     console.log(offset);
-    $('.header::before').css({
-        display: none;
+    var pad_r = $(window).width() - 54 -  $('.name_and_description').width() - offset.left;
+    var pad_b = $(".header").height() - 30 -  $('.name_and_description').height() - offset.top;
+    $('.header_blur').css({
+        "padding-left": offset.left,
+        "padding-top": offset.top,
+        "padding-right": pad_r,
+        "padding-bottom": pad_b
     });
 });
